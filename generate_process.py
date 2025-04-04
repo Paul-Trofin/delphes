2fc49cde06757c70c46f6052ada04a1033a56904
Paul-Trofin <157028893+Paul-Trofin@users.noreply.github.com>
2025-04-04 16:54:57 +0300

    Update generate_process.py

--- generate_process.py
+++ generate_process.py
@@ -3,16 +3,16 @@
 ##### This script generates files for ./DelphesPythia8 #######
 ##############################################################
 import os
-                                                           #")
+                                                        
 ##############################################################
 ######################## OPTIONS #############################
 ##############################################################
 ### NAME 
-name = "ff_ZZ"
+name = "ff_z"
 ### PROCESS 
-process = "f f~ > gm > e- e+"
+process = "..."
 ### NUMBER OF EVENTS
-Nevents = "10000"
+Nevents = "60"
 ### BEAM SETTINGS
 idA = "2212"
 idB = "2212"
@@ -21,15 +21,17 @@ eCM = "13600"
 options = []
 options.append("")
 options.append("! Hard Process")
-options.append("WeakDoubleBoson:ffbar2gmZgmZ = on")
-options.append("WeakZ0:gmZmode = 2 ! include only gamma decays")
+options.append("WeakSingleBoson:ffbar2gmZ = on")
+
+# Weak gmZ Mode
+options.append("WeakZ0:gmZmode = 2 ! include only Z decays")
 
 ### PARTON LEVEL
 options.append("")
 options.append("! Parton Level")
-options.append("PartonLevel:MPI = off")
-options.append("PartonLevel:ISR = on")
-options.append("PartonLevel:FSR = on")
+options.append("PartonLevel:MPI = on")
+options.append("PartonLevel:ISR = off")
+options.append("PartonLevel:FSR = off")
 
 ### HADRON LEVEL
 options.append("")
@@ -37,40 +39,62 @@ options.append("! Hadron Level")
 options.append("HadronLevel:Hadronize = on")
 
 ### DECAY OPTIONS
+
+
 options.append("")
 options.append("! Force gamma decays to e- e+")
-options.append("22:onMode = off")
-options.append("22:onIfAny = 11 -11")
-options.append("")
-options.append("! Force Z decays to e- e+")
 options.append("23:onMode = off")
-options.append("23:onIfAny = 11 -11")
+options.append("23:onIfAll = 11 -11")
+#options.append("")
+#options.append("! Force W-+ decays to e-+ ve+-")
+#options.append("24:onMode = off")       
+#options.append("24:onIfAll = -11 -12")    
+#options.append("-24:onMode = off")      
+#options.append("-24:onIfAll = 11 12")
+
+#options.append("")
+#options.append("! Force mu-+ decays to ")
+#options.append("13:onMode = off")
+#options.append("13:onIfAll = 11 -12 14")
+#options.append("-13:onMode = off")
+#options.append("-13:onIfAll = -11 12 -14")
+#options.append("")
+#options.append("! Force ta-+ decays to e-+ ve+- vt-+")
+#options.append("15:onMode = off")
+#options.append("15:onIfAny = 11 -12 16")
+#options.append("-15:onMode = off")
+#options.append("-15:onIfAny = -11 12 -16")
+
+#options.append("")
+#options.append("! Force top-quark decays to W b")
+#options.append("6:onMode = off")
+#options.append("6:onIfAny = 24 5")
+
+
+
+
 options.append("")
-options.append("! Force W-+ decays to e-+ ve+-")
-options.append("24:onMode = off")       
-options.append("24:onIfAny = 11 12")    
-options.append("-24:onMode = off")      
-options.append("-24:onIfAny = -11 -12")
+
 
 ### PHASE SPACE CUTS
 options.append("")
 #options.append("PhaseSpace:pTHatMin = 10.0") 
 ##############################################################
 print(" ______________________________________________________________")
-print("|                                                              |")
-print("|                ___________________________                   |")
-print("|               |                           |                  |")
-print("|               | DelphesPythia8 Simulation |                  |")
-print("|               |___________________________|                  |")
-print("|                                                              |")
-print("|                                                              |")
-print("|            Colliding: " + idA + " + " + idB + " at " + eCM + " GeV" + "               |")
-print("|                                                              |")
-print("|                      Process: " + process)
-print("|                                                              |")
-print("|                                                              |")
-print("|                     AUTHOR : PAUL TROFIN                     |")
-print("\______________________________________________________________/")
+print("                                                              ")
+print("                ___________________________                   ")
+print("               |                           |                  ")
+print("               | DelphesPythia8 Simulation |                  ")
+print("               |___________________________|                  ")
+print("                                                              ")
+print("                                                              ")
+print("            Colliding: " + idA + " + " + idB + " at " + eCM + " GeV")
+print("                                                              ")
+print("                      Process: " + process)
+print("                                                              ")
+print("                                                              ")
+print("                     AUTHOR : PAUL TROFIN                     ")
+print("_______________________________________________________________")
 
 # CREATE FOLDER
 folder_path = name
@@ -180,13 +204,12 @@ void get_variables(const char *inputFile) {{
     }};
 
     int N_electrons = 0, N_positrons = 0, N_electron_pairs = 0, N_jets = 0;
-    double m_ee = 0, deltaR_ee = 0, MET = 0, Zjet_phi = 0, jets_mass = 0, best_deltaR_ee = 0, best_Z_pT = 0;
+    double m_ee = 0, DielectronDeltaR = 0, MET = 0, Zjet_phi = 0, jets_mass = 0;
 
 	TTree* Electrons = new TTree("Electrons", "");
     TTree* Positrons = new TTree("Positrons", "");
     TTree* ElectronPairs = new TTree("Dielectrons", "");
     TTree* Jets = new TTree("Jets", "");
-    TTree* BDT = new TTree("BDT", "");
 
     // Main branches for the number of entries
     Electrons->Branch("NumberOfElectrons", &N_electrons, "N_electrons/I");
@@ -195,22 +218,19 @@ void get_variables(const char *inputFile) {{
     Jets->Branch("JetMultiplicity", &N_jets, "N_jets/I");
 
     // Sub-branches
-    P4 e_p4, e_bar_p4, Z_p4, jet_p4;
-    Angle e_angle, e_bar_angle, Z_angles, jet_angle;
+    P4 e_p4, e_bar_p4, Dielectron_p4, jet_p4;
+    Angle e_angle, e_bar_angle, Dielectron_angle, jet_angle;
     Electrons->Branch("electron_p4", &e_p4, "E/D:Px/D:Py/D:Pz/D:Pt/D");
     Electrons->Branch("electron_angle", &e_angle, "eta/D:phi/D:theta/D");
     Positrons->Branch("positron_p4", &e_bar_p4, "E/D:Px/D:Py/D:Pz/D:Pt/D");
     Positrons->Branch("positron_angle", &e_bar_angle, "eta/D:phi/D:theta/D");
     ElectronPairs->Branch("DielectronMass", &m_ee, "m_ee/D");
-    ElectronPairs->Branch("deltaR_ee", &deltaR_ee, "deltaR_ee/D");
-    ElectronPairs->Branch("Z_p4", &Z_p4, "E/D:Px/D:Py/D:Pz/D:Pt/D");
-    ElectronPairs->Branch("Z_angles", &Z_angles, "eta/D:phi/D:theta/D");
+    ElectronPairs->Branch("DielectronDeltaR", &DielectronDeltaR, "DielectronDeltaR/D");
+    ElectronPairs->Branch("Dielectron_p4", &Dielectron_p4, "E/D:Px/D:Py/D:Pz/D:Pt/D");
+    ElectronPairs->Branch("Dielectron_angle", &Dielectron_angle, "eta/D:phi/D:theta/D");
     Jets->Branch("jet_p4", &jet_p4, "E/D:Px/D:Py/D:Pz/D:Pt/D");
     Jets->Branch("jet_angle", &jet_angle, "eta/D:phi/D:theta/D");
     Jets->Branch("JetMultiplicity", &N_jets, "N_jets/I");
-    BDT->Branch("best_deltaR_ee", &best_deltaR_ee, "best_deltaR_ee/D");
-    BDT->Branch("best_Z_pT", &best_Z_pT, "best_Z_pT/D"); 
-    BDT->Branch("JetMultiplicity", &N_jets, "N_jets/I");
     
 
     //////////////////////////////////////////////////////////////////////
@@ -255,29 +275,21 @@ void get_variables(const char *inputFile) {{
 
         //////////////////////////////////////////////////////////////////////
         //////////////////////////////// PAIRS ///////////////////////////////
-        //////////////////////////////////////////////////////////////////////
-        TLorentzVector best_Z;        
+        //////////////////////////////////////////////////////////////////////       
         for (auto e : Selectrons) {{
             for (auto e_bar : Spositrons) {{
-                TLorentzVector Z = (e + e_bar);
-                m_ee = Z.M();
-                Z_p4.E = Z.E();
-                Z_p4.Px = Z.Px();
-                Z_p4.Py = Z.Py();
-                Z_p4.Pz = Z.Pz();
-                Z_p4.Pt = Z.Pt();
-                Z_angles.eta = Z.Eta();
-                Z_angles.phi = std::abs(e.Phi() - e_bar.Phi());
-                Z_angles.theta = Z.Theta();
-                deltaR_ee = std::sqrt((e.Eta() - e_bar.Eta())*(e.Eta() - e_bar.Eta()) + (e.Phi() - e_bar.Phi())*(e.Phi() - e_bar.Phi()));
+                TLorentzVector diel = (e + e_bar);
+                m_ee = diel.M();
+                Dielectron_p4.E = diel.E();
+                Dielectron_p4.Px = diel.Px();
+                Dielectron_p4.Py = diel.Py();
+                Dielectron_p4.Pz = diel.Pz();
+                Dielectron_p4.Pt = diel.Pt();
+                Dielectron_angle.eta = diel.Eta();
+                Dielectron_angle.phi = std::abs(e.Phi() - e_bar.Phi());
+                Dielectron_angle.theta = diel.Theta();
+                DielectronDeltaR = std::sqrt((e.Eta() - e_bar.Eta())*(e.Eta() - e_bar.Eta()) + (e.Phi() - e_bar.Phi())*(e.Phi() - e_bar.Phi()));
                 ElectronPairs->Fill();
-
-                // Store best electron positron pair for BDT
-                if (std::abs(Z.M() - 91.2) < std::abs(best_Z.M() - 91.2)) {{
-                    best_Z = Z;
-                    best_deltaR_ee = deltaR_ee;
-                    best_Z_pT = Z.Pt();
-                }}
             }}
         }}
 
@@ -309,7 +321,6 @@ void get_variables(const char *inputFile) {{
             
         }}
 
-        BDT->Fill();
     }}
 
     
@@ -319,7 +330,6 @@ void get_variables(const char *inputFile) {{
     Positrons->Write();
     ElectronPairs->Write();
     Jets->Write();
-    BDT->Write();
     outFile.Close();
 
     // Clean up
